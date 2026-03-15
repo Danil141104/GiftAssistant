6caeacfc16de5facd67fe6a2a61889b97127a6fb
Popov Danil <dppopov@edu.hse.ru>
2026-03-15 21:45:51 +0300

    Initial Commit

---
+++ GiftAssistant/GiftAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftAssistantApp.swift
+//  GiftAssistant
+//
+//  Created by Данил Попов on 15.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct GiftAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
